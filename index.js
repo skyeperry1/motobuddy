855494313e4f87d18493add2acad39e38a020eed
Skye Perry <skyeperry1@gmail.com>
2016-06-27 14:01:02 -0400

    built basic site

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var server = require('http').createServer(app);
 app.use(express.static(__dirname + '/public_html'));
 
 
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/index.html');
+});
+
+
+app.get('/ma-practice-test', function(req, res){
+  res.sendfile(__dirname + '/ma-practice-test.html');
+});
+
+
 
 server.listen(process.env.PORT || 5000, function(){
   console.log('listening on port:5000');
